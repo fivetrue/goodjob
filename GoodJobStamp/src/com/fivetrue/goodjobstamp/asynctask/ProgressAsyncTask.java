4aa93fc2e1412fcd1332d86d84e748d173a2740a
fivetrue <fivetrue1101@gmail.com>
2014-06-10 23:18:28 +0900

    ProgressAsyncTask

--- GoodJobStamp/src/com/fivetrue/goodjobstamp/asynctask/ProgressAsyncTask.java
+++ GoodJobStamp/src/com/fivetrue/goodjobstamp/asynctask/ProgressAsyncTask.java
@@ -1,14 +1,22 @@
 package com.fivetrue.goodjobstamp.asynctask;
 
+import com.example.goodjobstamp.R;
+
 import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public abstract class ProgressAsyncTask extends	AsyncTask<Object, Object, Object> {
 	private Activity mActivity = null;
 	private LayoutInflater mInflater = null;
-	
+	private ViewGroup mView = null;
+	private TextView mTvProgress = null;
+	private Dialog mDialog = null;
+
 	public ProgressAsyncTask(Activity activity){
 		this.mActivity = activity;
 		this.mInflater = (LayoutInflater) mActivity.getLayoutInflater();
@@ -18,16 +26,21 @@ public abstract class ProgressAsyncTask extends	AsyncTask<Object, Object, Object
 	protected void onPreExecute() {
 		// TODO Auto-generated method stub
 		super.onPreExecute();
-//		ViewGroup view = (ViewGroup) this.mInflater.inflate(R.layout.inc_progress, null);
+		
+		mView = (ViewGroup) this.mInflater.inflate(R.layout.inc_progress, null);
+		mTvProgress = (TextView) mView.findViewById(R.id.tv_progress);
+		mTvProgress.setText(setTextToProgress());
 		if(isShowing()){
 			if(mergeToParent()){
-				
-			}else{
-				if(canDissmiss()){
-					
-				}else{
-					
+				if(getParentView() != null){
+					getParentView().addView(mView);
 				}
+			}else{
+				mDialog = new Dialog(mActivity);
+				mView.setBackgroundColor(canDissmiss() ? Color.TRANSPARENT : Color.GRAY);
+				mDialog.setContentView(mView);
+				mDialog.setCanceledOnTouchOutside(canDissmiss());
+				mDialog.show();
 			}
 		}
 	}
@@ -42,12 +55,19 @@ public abstract class ProgressAsyncTask extends	AsyncTask<Object, Object, Object
 	protected void onPostExecute(Object result) {
 		// TODO Auto-generated method stub
 		super.onPostExecute(result);
+		
+		if(mDialog != null)
+			mDialog.dismiss();
+		
+		if(mView != null)
+			getParentView().removeView(mView);
+			
 	}	
-	
+
 	abstract protected boolean isShowing();
 	abstract protected boolean canDissmiss();
 	abstract protected boolean mergeToParent();
 	abstract protected ViewGroup getParentView();
 	abstract protected String setTextToProgress();
-	
+
 }
