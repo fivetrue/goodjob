c9dfc33c3547a36c5e691b750fc10a53b1c220d2
fivetrue <fivetrue1101@gmail.com>
2014-06-11 17:19:06 +0900

    Config

---
+++ GoodJobStamp/src/com/fivetrue/goodjobstamp/common/Config.java
@@ -0,0 +1,9 @@
+package com.fivetrue.goodjobstamp.common;
+
+public class Config {
+	static public String GOOGLE_PROJECT_NAME = "lucky-trail-608";
+	static public String GOOGLE_PROJECT_ID = "650874166288";
+	
+	static public String GOOGLE_CLIENT_ID = "650874166288-ll26u0c2acp8p8kmoov6eis9280vjug8.apps.googleusercontent.com";
+	static public String GOOGLE_EMAIL_ADDRESS = "650874166288-ll26u0c2acp8p8kmoov6eis9280vjug8@developer.gserviceaccount.com";
+}
